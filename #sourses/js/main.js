1c4da5f199a25bac2e33a9707b9bb589b724b721
nicotini <elenafreim@gmail.com>
2022-06-21 01:12:59 +0200

    made corrections th the header

--- #sourses/js/main.js
+++ #sourses/js/main.js
@@ -1,14 +1,17 @@
 /**burger handler */
 (function() {
+    const overlay = document.querySelector('.overlay');
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
     const menuClose = document.querySelector('.header__nav-close');
     const menuLinkls = document.querySelectorAll('.header__link');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav-active');
+        overlay.classList.add('overlay-active');
     });
     menuClose.addEventListener('click', () => {
         menu.classList.remove('header__nav-active');
+        overlay.classList.remove('overlay-active');
     });
     if(window.innerWidth <= 766) {
         for(let i = 0; i < menuLinkls.length; i+= 1) {
