84e94091380d97b415ab642dff489b3b1e4512df
nicotini <elenafreim@gmail.com>
2022-06-21 11:39:39 +0200

    fixed some problems with header

--- #sourses/js/main.js
+++ #sourses/js/main.js
@@ -1,3 +1,14 @@
+(function() {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if(window.pageYOffset > 50) {
+            header.classList.add('header__active');
+        } else {
+            header.classList.remove('header__active');
+        }
+    }
+
+}());
 /**burger handler */
 (function() {
     const overlay = document.querySelector('.overlay');
